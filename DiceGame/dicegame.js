fd81334a64a16d30d10cc24c6bed634208f373d1
Kasper Syvertsen <70322797+Kaspersyv@users.noreply.github.com>
2021-11-30 12:22:21 +0100

    Test code js

--- DiceGame/dicegame.js
+++ DiceGame/dicegame.js
@@ -1,1 +1,24 @@
 
+//RNG for the dice images
+let randomNumber1 = Math.floor(Math.random() * (1, 6) + 1)
+let randomNumber2 = Math.floor(Math.random() * (1, 6) + 1)
+
+
+//Changing the dices according to the RNG number
+document.getElementById("img1").setAttribute('src', 'images/dice'+ randomNumber1 + '.png')
+document.getElementById("img2").setAttribute('src', 'images/dice'+ randomNumber2 + '.png')
+
+if (randomNumber1 == randomNumber2) {
+    document.getElementById("result").innerText = "Draw!"
+}
+else if (randomNumber1 > randomNumber2) {
+    document.getElementById("result").innerText = "Player 1 wins!"
+}
+else if (randomNumber1 < randomNumber2) {
+    document.getElementById("result").innerText = "Player 2 wins!"
+}
+else {
+    alert("Error!")
+}
+
+
