31abbd22c79de64387b109fac8058335e538368d
Tor Grønås <tor.gronas@hotmail.com>
2021-12-14 11:13:11 +0100

    Update guess the number.js

--- GuessTheNumber/JS/guess the number.js
+++ GuessTheNumber/JS/guess the number.js
@@ -1,9 +1,13 @@
-var randomNumber1 = Math.floor(Math.random() * 100) + 1;
-// var userGuess = parseInt(document.getElementById("guess-input").value);
+alert("Hei");
 
-var userGuess = document.getElementById("guess-input").value;
+var randomNumber1 = Math.floor(Math.random() * 100) + 1;
+var userGuess = parseInt(document.getElementById("guess-input").value);
+
+// var userGuess = document.getElementById("guess-input").value;
 
 
 function GuessTheNumber() {
-    document.getElementById("title-guessthenumber").innerHTML= userGuess;
+    alert(userGuess);
+    alert(randomNumber1);
+    document.getElementById("title-guessthenumber").innerHTML = userGuess;
 }
