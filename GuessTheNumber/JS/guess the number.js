4816923763fd3f9403f3beb15ffc7e84d1be808e
Tor Grønås <tor.gronas@hotmail.com>
2022-01-03 14:49:42 +0100

    Guess the number spill (Utkast 1)

--- GuessTheNumber/JS/guess the number.js
+++ GuessTheNumber/JS/guess the number.js
@@ -1,13 +1,20 @@
-alert("Hei");
-
-var randomNumber1 = Math.floor(Math.random() * 100) + 1;
-var userGuess = parseInt(document.getElementById("guess-input").value);
-
-// var userGuess = document.getElementById("guess-input").value;
-
+let randomNumber1 = Math.floor(Math.random() * 100) + 1;
 
 function GuessTheNumber() {
-    alert(userGuess);
-    alert(randomNumber1);
+    let userGuess = document.getElementById("guess-input").value;
     document.getElementById("title-guessthenumber").innerHTML = userGuess;
+
+    if (userGuess > randomNumber1) {
+        document.getElementById("title-guessthenumber").innerHTML = userGuess + " er større enn svaret.";
+    }
+    else {
+        if (userGuess < randomNumber1) {
+            document.getElementById("title-guessthenumber").innerHTML = userGuess + " er mindre enn svaret.";
+        }
+        else {
+            document.getElementById("title-guessthenumber").innerHTML = userGuess + " er riktig!";
+            document.getElementById("button-div").innerHTML = '<button class="btn btn-guess btn-primary" onClick="window.location.reload();">Restart</button>';
+        }
+    }
+
 }
